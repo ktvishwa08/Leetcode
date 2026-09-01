b4119b9603679bc11af054a40db3b64b901d5317
ktvishwa08 <ktvishwa06@gmail.com>
2026-09-01 15:06:13 +0530

    Time: 17 ms (12.83%), Space: 47.1 MB (27.82%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
+        } 
+        for(int i=0;i<ransomNote.length();i++){
+            char ch=ransomNote.charAt(i);
+            if(!map.containsKey(ch)||map.get(ch)<=0){
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);
+        }
+        return true;
+    }
+}
